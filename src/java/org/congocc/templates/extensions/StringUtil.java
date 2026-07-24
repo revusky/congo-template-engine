09a9ebe06a335c74f0f7eb9b369891646a7b30fd
revusky <revusky@gmail.com>
2026-07-24 11:28:44 +0200

    further reorg

--- src/java/org/congocc/templates/extensions/StringUtil.java
+++ src/java/org/congocc/templates/extensions/StringUtil.java
@@ -1,8 +1,6 @@
 package org.congocc.templates.extensions;
 
-import java.io.UnsupportedEncodingException;
 import java.util.*;
-import org.congocc.templates.core.parser.ParseException;
 
 /**
  *  Some text related utilities.
@@ -65,126 +63,6 @@ public class StringUtil {
         return buf.length() == s.length() ? s : buf.toString();
     }
 
-    /**
-     * CTL string literal decoding.
-     *
-     * \\, \", \', \n, \t, \r, \b and \f will be replaced according to
-     * Java rules. In additional, it knows \g, \l, \a and \{ which are
-     * replaced with &lt;, >, &amp; and { respectively.
-     * \x works as hexadecimal character code escape. The character
-     * codes are interpreted according to UCS basic plane (Unicode).
-     * "f\x006Fo", "f\x06Fo" and "f\x6Fo" will be "foo".
-     * "f\x006F123" will be "foo123" as the maximum number of digits is 4.
-     *
-     * All other \X (where X is any character not mentioned above or End-of-string)
-     * will cause a ParseException.
-     *
-     * @param s String literal <em>without</em> the surrounding quotation marks
-     * @return String with all escape sequences resolved
-     * @throws ParseException if there string contains illegal escapes
-     */
-    public static String CTLStringLiteralDec(String s) {
-        int idx = s.indexOf('\\');
-        if (idx == -1) {
-            return s;
-        }
-        int lidx = s.length() - 1;
-        int bidx = 0;
-        StringBuilder buf = new StringBuilder(lidx);
-        do {
-            buf.append(s.substring(bidx, idx));
-            if (idx >= lidx) {
-                throw new ParseException("The last character of string literal is backslash", 0,0);
-            }
-            char c = s.charAt(idx + 1);
-            switch (c) {
-                case '"':
-                    buf.append('"');
-                    bidx = idx + 2;
-                    break;
-                case '\'':
-                    buf.append('\'');
-                    bidx = idx + 2;
-                    break;
-                case '\\':
-                    buf.append('\\');
-                    bidx = idx + 2;
-                    break;
-                case 'n':
-                    buf.append('\n');
-                    bidx = idx + 2;
-                    break;
-                case 'r':
-                    buf.append('\r');
-                    bidx = idx + 2;
-                    break;
-                case 't':
-                    buf.append('\t');
-                    bidx = idx + 2;
-                    break;
-                case 'f':
-                    buf.append('\f');
-                    bidx = idx + 2;
-                    break;
-                case 'b':
-                    buf.append('\b');
-                    bidx = idx + 2;
-                    break;
-                case 'g':
-                    buf.append('>');
-                    bidx = idx + 2;
-                    break;
-                case 'l':
-                    buf.append('<');
-                    bidx = idx + 2;
-                    break;
-                case 'a':
-                    buf.append('&');
-                    bidx = idx + 2;
-                    break;
-                case '{':
-                    buf.append('{');
-                    bidx = idx + 2;
-                    break;
-                case 'x': {
-                    idx += 2;
-                    int x = idx;
-                    int y = 0;
-                    int z = lidx > idx + 3 ? idx + 3 : lidx;
-                    while (idx <= z) {
-                        char b = s.charAt(idx);
-                        if (b >= '0' && b <= '9') {
-                            y <<= 4;
-                            y += b - '0';
-                        } else if (b >= 'a' && b <= 'f') {
-                            y <<= 4;
-                            y += b - 'a' + 10;
-                        } else if (b >= 'A' && b <= 'F') {
-                            y <<= 4;
-                            y += b - 'A' + 10;
-                        } else {
-                            break;
-                        }
-                        idx++;
-                    }
-                    if (x < idx) {
-                        buf.append((char) y);
-                    } else {
-                        throw new ParseException("Invalid \\x escape in a string literal",0,0);
-                    }
-                    bidx = idx;
-                    break;
-                }
-                default:
-                    throw new ParseException("Invalid escape sequence (\\" + c + ") in a string literal",0,0);
-            }
-            idx = s.indexOf('\\', bidx);
-        } while (idx != -1);
-        buf.append(s.substring(bidx));
-
-        return buf.toString();
-    }
-
     public static String capitalize(String s) {
         StringTokenizer st = new StringTokenizer(s, " \t\r\n", true);
         StringBuilder buf = new StringBuilder(s.length());
@@ -196,26 +74,6 @@ public class StringUtil {
         return buf.toString();
     }
 
-    public static boolean getYesNo(String s) {
-        if (s.startsWith("\"")) {
-            s = s.substring(1, s.length() -1);
-
-        }
-        if (s.equalsIgnoreCase("n")
-                || s.equalsIgnoreCase("no")
-                || s.equalsIgnoreCase("f")
-                || s.equalsIgnoreCase("false")) {
-            return false;
-        }
-        else if  (s.equalsIgnoreCase("y")
-                || s.equalsIgnoreCase("yes")
-                || s.equalsIgnoreCase("t")
-                || s.equalsIgnoreCase("true")) {
-            return true;
-        }
-        throw new IllegalArgumentException("Illegal boolean value: " + s);
-    }
-
     /**
      * Removes the line-break from the end of the string.
      */
@@ -223,50 +81,6 @@ public class StringUtil {
         if (s.endsWith("\r\n")) return s.substring(0, s.length() - 2);
         char lastChar = s.length() == 0 ? 0 : s.charAt(s.length()-1);
         return (lastChar != '\n' && lastChar != '\r') ? s : s.substring(0, s.length() - 1);
-    }
-
-    /**
-     * Quotes string as Java Language string literal.
-     * Returns string <code>"null"</code> if <code>s</code>
-     * is <code>null</code>.
-     */
-    public static String jQuote(String s) {
-        if (s == null) {
-            return "null";
-        }
-        int ln = s.length();
-        StringBuilder b = new StringBuilder(ln + 4);
-        b.append('"');
-        for (int i = 0; i < ln; i++) {
-            char c = s.charAt(i);
-            if (c == '"') {
-                b.append("\\\"");
-            } else if (c == '\\') {
-                b.append("\\\\");
-            } else if (c < 0x20) {
-                if (c == '\n') {
-                    b.append("\\n");
-                } else if (c == '\r') {
-                    b.append("\\r");
-                } else if (c == '\f') {
-                    b.append("\\f");
-                } else if (c == '\b') {
-                    b.append("\\b");
-                } else if (c == '\t') {
-                    b.append("\\t");
-                } else {
-                    b.append("\\u00");
-                    int x = c / 0x10;
-                    b.append((char) (x < 0xA ? x + '0' : x - 0xA + 'A'));
-                    x = c & 0xF;
-                    b.append((char) (x < 0xA ? x + '0' : x - 0xA + 'A'));
-                }
-            } else {
-                b.append(c);
-            }
-        } // for each characters
-        b.append('"');
-        return b.toString();
     }
 
     /**
